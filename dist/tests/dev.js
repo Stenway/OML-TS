ff06fb306f51433d5feb5e489617cf2e5a5eaebb
Stenway <79376889+Stenway@users.noreply.github.com>
2023-02-11 16:36:55 +0100

    Preparations for 0.2.0

--- dist/tests/dev.js
+++ dist/tests/dev.js
@@ -2,5 +2,17 @@
 /* eslint-disable no-console */
 Object.defineProperty(exports, "__esModule", { value: true });
 const src_1 = require("../src");
-console.log(src_1.Oml.stringify({ test: true, test2: "Hello world" }));
+const obj = {
+    key1: 123,
+    key2: true,
+    key3: "Hello world",
+    key4: null,
+    key5: [0, 10, 20],
+    key6: {
+        width: 1920,
+        height: 1080
+    }
+};
+console.log("JSON: " + JSON.stringify(obj));
+console.log("OML:  " + src_1.Oml.stringify(obj));
 //# sourceMappingURL=dev.js.map
--- tests/dev.ts
+++ tests/dev.ts
@@ -2,4 +2,17 @@
 
 import { Oml } from "../src"
 
-console.log(Oml.stringify({test: true, test2: "Hello world"}))
+const obj = {
+	key1: 123,
+	key2: true,
+	key3: "Hello world",
+	key4: null,
+	key5: [0, 10, 20],
+	key6: {
+		width: 1920,
+		height: 1080
+	}
+}
+
+console.log("JSON: "+JSON.stringify(obj))
+console.log("OML:  "+Oml.stringify(obj))
